0161df8340b320939ca9d6a59ad47b640fd3d3bb
Bach Dao <ngocbach12a1@gmail.com>
2025-07-17 23:27:11 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from "react-router-dom";
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
